2e2a38ec4d084203574a9b8ee7eed951c1be04f0
Priyesh <priyeshpawar07@gmail.com>
2018-08-19 23:01:52 +0530

    check all by default and share details

---
+++ app/src/main/java/in/oriange/iblebook/activities/ShareBankDetails_Activity.java
@@ -0,0 +1,16 @@
+package in.oriange.iblebook.activities;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.oriange.iblebook.R;
+
+public class ShareBankDetails_Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_bank_details);
+    }
+}
---
+++ app/src/main/java/in/oriange/iblebook/activities/ShareGSTDetails_Activity.java
@@ -0,0 +1,16 @@
+package in.oriange.iblebook.activities;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.oriange.iblebook.R;
+
+public class ShareGSTDetails_Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_gst_details);
+    }
+}
